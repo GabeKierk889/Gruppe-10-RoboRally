b280d34c3ff5d550c0806b3a8e75e6d01ed820f9
Xiao Chen <xchen2305@gmail.com>
2022-04-27 00:04:54 +0200

    updated with new files

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ConveyorBelt.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ConveyorBelt.java
@@ -1,3 +1,25 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+import org.jetbrains.annotations.NotNull;
+
 public class ConveyorBelt extends FieldAction {
+
+    private Heading heading;
+
+    public Heading getHeading() {
+        return heading;
+    }
+
+    public void setHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+    @Override
+    public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
+        // TODO needs to be implemented
+        return false;
+    }
+
+}
